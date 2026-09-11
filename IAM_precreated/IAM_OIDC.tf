31a3d8aa2690e14ca00f4d160021189306bfa3fc
saucisse-groumande <kawamura.itsuki.work@gmail.com>
2026-09-11 22:54:02 +0100

    add README file

---
+++ IAM_precreated/IAM_OIDC.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:<your-account-name>@<owner-id>/<repository>@<repositoryid>:*"
+                                              # example "repo:itsuki-kawamura-dev@1234567890/aaaaaaaaaaaa@1234567890:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
